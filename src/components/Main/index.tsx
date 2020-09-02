90491ab090682c692ed6058dcc503e4c3a8ebc65
dabisilvaond@gmail.com <dabisilvaond@gmail.com>
2020-09-02 17:59:38 -0300

    inicio de clone do twiter, com 1 responsividade feita parcialmente

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon/>
+        </button>
+
+        <ProfileInfo>
+          <strong>Davi Barbosa</strong>
+          <span>75 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage/> */}
+
+      <BottomMenu>
+        <HomeIcon className="active"/>
+        <SearchIcon/>
+        <BellIcon/>
+        <EmailIcon/>
+      </BottomMenu>
+    </Container>
+  );
+}
+
+export default Main;
